0a10754e96886d55bb4150e1026042fa6d59de2c
RKJenamani <rajatkj11@gmail.com>
2019-12-08 04:57:44 +0530

    Question 4

---
+++ codeforces/ladder-d/4.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lf long double
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+typedef pair <ll,ll> ii;
+typedef vector <ll> vi;
+typedef vector <ii> vii;
+typedef vector <bool> vb;
+typedef vector <vector <ll> > vv;
+typedef vector <string> vs;
+typedef vector <lf> vf;
+#define mod 1000000007
+#define inf 999999999999999ll
+ 
+int main()
+{
+    ll a;
+    cin>>a;
+    if(a%2==0)
+        cout<<"white"<<endl<<1<<" "<<2<<endl;
+    else
+        cout<<"black"<<endl;
+    return 0;
+}
